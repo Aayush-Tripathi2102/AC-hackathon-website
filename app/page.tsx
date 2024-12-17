760b3bc082ad89211de267ecd75475e73967c77b
StephenStolk <shikharburman2@gmail.com>
2024-12-17 22:28:52 +0530

    Removed unwanted variables

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { Navbar } from "@/components/navbar"
 import { Hero } from "@/components/hero"
 import { MetaverseSection } from "@/components/metaverse-section"
 import { AIImageGallery } from "@/components/ai-image-gallery"
-import { Stats } from "@/components/stats"
+// import { Stats } from "@/components/stats"
 import { Schedule } from "@/components/schedule"
-import { Speakers } from "@/components/speakers"
+// import { Speakers } from "@/components/speakers"
 import { PrizesSection } from "@/components/prizes-section"
 import { ConferencePasses } from "@/components/conference-passes"
-import { PastProjects } from "@/components/past-projects"
-import ImageGallery from "@/components/ImageGallery";
+// import { PastProjects } from "@/components/past-projects"
+// import ImageGallery from "@/components/ImageGallery";
 import Tempo from "@/components/tempo";
 import Androidinfo from "@/components/androidinfo";
 
